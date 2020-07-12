339799670dd686028110d8c4dd5f457f00fa77cd
Sergey Redko <ser_igel@ip-172-31-29-212.us-east-2.compute.internal>
2020-07-12 04:58:44 +0000

    m -> message

--- commands/promote.js
+++ commands/promote.js
@@ -9,7 +9,7 @@ module.exports = {
             console.log("somebody tried to hack the system")
             message.reply(`вы уже должны быть преподавателем или волонтёром, чтобы давать роль преподавателя или волонтёра другим.`)
         } else {
-            const taggedUser = message.guild.members.cache.get(m.mentions.users.first().id)
+            const taggedUser = message.guild.members.cache.get(message.mentions.users.first().id)
             taggedUser.roles.add(message.member.roles.highest)
         }
 	},
