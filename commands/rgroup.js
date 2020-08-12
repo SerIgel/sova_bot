2c6d5042484430807b90ba023d718900af73ac7e
Sergey Redko <sergey@redko.me>
2020-08-12 06:02:23 +0300

    fix: !rgroup Ученик

--- commands/rgroup.js
+++ commands/rgroup.js
@@ -7,6 +7,7 @@ module.exports = {
     usage: '<имя группы>',
 	execute(message, args) {
         if (!group_channel.includes(message.channel.id)) {  return message.delete();}
+        if (!args[0].match(/^\d/)) {return message.delete();}
         let role = message.guild.roles.cache.find(r => r.name === args.join(' '));
         if (!role) {
             return message.reply(`группы ${args.join(' ')} не существует`)
