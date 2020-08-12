caea9cfc2e161bc75ae068bc2ccd0bd14e4c30b0
Sergey Redko <sergey@redko.me>
2020-08-12 07:30:03 +0300

    fix: permissions

--- commands/reset.js
+++ commands/reset.js
@@ -5,6 +5,9 @@ module.exports = {
     description: 'Reset teams',
     async execute(message, args) {
         message.guild.members.cache.forEach(async member => {
+            if (!["Волонтёр", "Преподаватель"].includes(message.member.roles.highest.name) && (message.member.id !== admin)) {
+                return message.reply(`вы должны быть преподавателем, волонтёром или мной, сбрасывать команды`)
+            }
             let role = member.roles.cache.find(r => r.name.includes("Команда"))
             if (!role) { return; }
             await member.roles.remove(role)
